495c9bbdb931ec167b24dee7a4e1dd745acf56d3
Marcel Castellani Aldecoa <mcastellani.aldecoa@outlook.com>
2017-09-26 22:31:48 -0300

    Initial structure.

---
+++ ArrayAndStrings/IsUnique.py
@@ -0,0 +1,16 @@
+########
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot
+# use additional data structures?
+########
+# Hints
+#44 Try a hashtable
+#117 Could a bit vector be useful?
+#132 Can you solve it in O(N log N) time? What might a solution like that look like?
+
+import numpy as np
+
+def IsUnique(s):
+    l = len(s)
+    return l
+
+print(IsUnique("abc"))
