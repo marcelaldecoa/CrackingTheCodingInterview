d6bf14a7d526a5c9fcfe50c0ded249b0df777adc
Bruno Oliveira <brolivei@microsoft.com>
2017-09-28 20:25:10 -0300

    Interview Question 1.1 - Isunique: new optimized version

--- CSharp/ArrayAndStrings/IsUnique.cs
+++ CSharp/ArrayAndStrings/IsUnique.cs
@@ -14,46 +14,41 @@ namespace ArrayAndStrings {
     class IsUnique {
         public static void Run(){
             //Prepare input
-            var inputSize = 256;
-            var input = new char[inputSize];
-
-            for (int i=0; i < inputSize; i++) {
-                input[i] = (char)i;    
-            }
+            var input ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus lectus eros, sit amet facilisis nibh tempor vulputate. Fusce scelerisque pharetra risus, non varius ante mollis non. Nam turpis mauris, vehicula quis rhoncus sit amet, egestas efficitur leo. Donec eget leo laoreet turpis duis. ";
 
 
             System.Diagnostics.Stopwatch watch = System.Diagnostics.Stopwatch.StartNew();
  
-            Optimized(new String(input));
+          
+            var  isUnique = BruteForce(input);
             watch.Stop();
+            Console.WriteLine($"Brute Force - The text has unique characteres: {isUnique}");
             Console.WriteLine($"Elapsed Time: { watch.Elapsed}");
             
             watch = System.Diagnostics.Stopwatch.StartNew();  
-            BruteForce(new String(input)); 
+
+            isUnique = Optimized(input);
             watch.Stop();
+            Console.WriteLine($"Optimized - The text has unique characteres: {isUnique}");
             Console.WriteLine($"Elapsed Time: { watch.Elapsed}");
         }
 
 
         //Target Time Complexity: O(n log n)
         //Time Complexity: ????
-        static void Optimized(string input){
+        static bool Optimized(string input){
             
             System.Collections.Hashtable characterMap = new System.Collections.Hashtable();
-           
-            //Para cada caracter da string exceto o primeiro caracter
+
             for (int i = 0; i < input.Length; i++) {
                 characterMap[input[i]] = 1;
             }
 
-            bool isUnique = input.Length == characterMap.Count;
-            
-            Console.WriteLine($"Optimized - The text has unique characteres: {isUnique}");
+           return input.Length == characterMap.Count;
         }
 
         //Time Complexity: O(n^2)
-        static void BruteForce(string input){
-            bool isUnique = true;
+        static bool BruteForce(string input){
             //Para cada caracter da string
             for (int outer = 0; outer < input.Length; outer++) {
 
@@ -62,19 +57,15 @@ namespace ArrayAndStrings {
                 //Percorre todos os outros caracteres para verificar se é unico
                 while (inner < input.Length) {
                     if(input[outer] == input[inner]) {
-                        isUnique = false;
-                        break;
+                        return false;
                     }
 
                     inner++;
-                }                            
-
-                if(!isUnique){
-                    break;
-                }
+                }        
             }
 
-            Console.WriteLine($"Brute Force - The text has unique characteres: {isUnique}");
+            return true;
+            
         }
     }
 }
